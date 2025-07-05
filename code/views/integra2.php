d1f1c10d74d85fe846c20171e58da4404e6c6376
Galo Izquierdo V <36722870+gizquier@users.noreply.github.com>
2025-07-05 11:45:54 -0500

    aw

---
+++ code/views/integra2.php
@@ -0,0 +1,168 @@
+<?php
+include_once dirname(__FILE__) . '/../inc/config.php';
+include_once dirname(__FILE__) . '/../inc/ErrCod.php';
+require_once dirname(__FILE__) . '/../inc/GenFunc.php';
+GenFunc::logSys("(integra) I:Ingreso en opcion");
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Integrantes</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+            <li class="breadcrumb-item active">Integrantes</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <!-- Default box -->
+    <div class="card card-solid">
+      <div class="card-body pb-0">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                Backend
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>Galo Izquierdo V.</b></h2>
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-mail-bulk"></i></span> Email: galo.izquierdov@ug.edu.ec</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-info"></i></span> Grupo C - Construccion de Software</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-info"></i></span> Grupo G-04 - Aplicaciones Web</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/giv.jpg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                fronted
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>Guevara Bustos Yandri David</b></h2>
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Email: yandridavid@hotmail.com</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Grupo C: Construcción de Software</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-info"></i></span> Grupo G05 - Aplicaciones Web</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/Yandri.jpeg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                fronted
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>Delgado Piguave Anthony Paul</b></h2>
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Email: anthony.delgadopig@ug.edu.ec</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Grupo C: Construcción de Software</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-info"></i></span> Grupo G04 - Aplicaciones Web</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/Pau.jpeg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                Documenter
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>IÑIGA ALVARADO STEVEN JOSUE</b></h2>
+                 
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Email: steven.inigaa@ug.edu.ec</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Grupo C: Construcción de Software</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-info"></i></span> Grupo G05 - Aplicaciones Web</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/Steven.jpeg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                Documenter
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>MITE SOLORZANO ALAN OMAR </b></h2>
+                 
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                       <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Email: alan.mitesol@ug.edu.ec</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Grupo C: Construcción de Software</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/Omar.jpeg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+            <div class="card bg-light d-flex flex-fill">
+              <div class="card-header text-muted border-bottom-0">
+                Documenter
+              </div>
+              <div class="card-body pt-0">
+                <div class="row">
+                  <div class="col-7">
+                    <h2 class="lead"><b>CAGUA LUCIO DEREK GEANPIERE</b></h2>
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Emai: derek.cagualuc@ug.edu.ec</li>
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Grupo C: Construcción de Software</li>
+                    </ul>
+                  </div>
+                  <div class="col-5 text-center">
+                    <img src="/img/Dereck.jpeg" alt="user-avatar" class="img-circle img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
