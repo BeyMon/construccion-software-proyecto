750cc6034d22c2ff83e5caa14827bb78041933d5
Galo Izquierdo V <36722870+gizquier@users.noreply.github.com>
2025-07-05 11:47:46 -0500

    cs

--- code/views/integra1.php
+++ code/views/integra1.php
@@ -84,7 +84,7 @@ GenFunc::logSys("(integra) I:Ingreso en opcion");
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Integrantes</h1>
+          <h1>Integrantes Grupo - Construccion de Software</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
--- code/views/tutor1.php
+++ code/views/tutor1.php
@@ -10,7 +10,7 @@ GenFunc::logSys("(integra) I:Ingreso en opcion");
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Tutor</h1>
+          <h1>Tutor - Construccion de Software</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-end">
